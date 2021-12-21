412ac539288072251bd49f9a71bc22a69e682767
regcs <reg.cs@t-online.de>
2021-12-21 16:23:35 +0100

    Patch version increase.

--- __init__.py
+++ __init__.py
@@ -21,7 +21,7 @@
 bl_info = {
 	"name": "Alice/LG",
 	"author": "Christian Stolze",
-	"version": (2, 0, 4),
+	"version": (2, 0, 5),
 	"blender": (2, 93, 6),
 	"location": "View3D > Looking Glass Tab",
 	"description": "Alice/LG takes your artworks through the Looking Glass (lightfield displays)",
