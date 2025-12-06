ce3d3f7819c5c8ad414bfcceeb5f9414c989c997
regcs <reg.cs@t-online.de>
2025-12-06 02:04:08 +0100

    Updating the version number to 2.3.1

--- __init__.py
+++ __init__.py
@@ -21,7 +21,7 @@
 bl_info = {
 	"name": "Alice/LG",
 	"author": "Christian Stolze",
-	"version": (2, 3, 0),
+	"version": (2, 3, 1),
 	"blender": (2, 93, 6),
 	"location": "View3D > Looking Glass Tab",
 	"description": "Alice/LG takes your artworks through the Looking Glass (light field displays)",
